4f8d2fa31fe49e2eb0ce8a0b2df92b0bfeb67b5e
Daniel Waksman <dwaksman@gmail.com>
2015-02-02 09:55:33 -0200

    Integration with NodeJs

--- www/js/services.js
+++ www/js/services.js
@@ -2,18 +2,21 @@ angular.module('starter.services', [])
 
 // FOR BROADCASTING MESSAGES
 .factory('Messages', function() {
+	
+	
 
-	var messages = [{
-		id: 0,
-		title: "Actualización disponible",
-		body: "Hay una actualizacion disponible para su(s) dispositivo(s)",
-		css_class: ""
-	}, {
-		id: 1,
-		title: "Estado del sistema",
-		body: "Todos los dispositivos funcionando normalmente",
-		css_class: ""
-	}];
+	var messages = [];
+
+	console.log(messages);
+
+	$.ajax({
+	    async: false,
+	    type: 'GET',
+	    url: 'http://dwaxmbook.local:5000/msgs',
+	    success: function(data) {
+	      messages = JSON.parse(data);
+	  	}
+	})
 
 	return {
 
@@ -32,35 +35,18 @@ angular.module('starter.services', [])
 /*SERVICE TO RETURN ROOM DATA
   to be changed upon client
 */
-.factory('Rooms', function( $http ) {
+.factory('Rooms', function() {
 
 	var rooms = [];
-
-	
-	// $http.get('http://dwaxmbook.local:5000/user/dani').
-	//   success(function(data, status, headers, config) {
-	//     rooms = JSON.parse(data);
-	//   }).
-	//   error(function(data, status, headers, config) {
-	//     // called asynchronously if an error occurs
-	//     // or server returns response with an error status.
-	//   });
-
-	// $.getJSON('http://dwaxmbook.local:5000/user/dani', function (data) {
-	// 	rooms = JSON.parse(data);
-	// })
-
-	// $.get('http://dwaxmbook.local:5000/user/dani').done(function(data) {
-	// 	rooms = data;
-	// });
 
 	$.ajax({
 	    async: false,
 	    type: 'GET',
-	    url: 'http://dwaxmbook.local:5000/user/dani',
+	    url: 'http://dwaxmbook.local:5000/rooms',
 	    success: function(data) {
 	      rooms = JSON.parse(data);
 	    }
+
 	  });
 
 	return {
